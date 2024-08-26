2cc19ccdbfa0f0109e8bbc0734ab2808b0fa1334
ayeayecapitan <95375383+ayeayecapitan@users.noreply.github.com>
2024-08-26 23:54:06 +0200

    Ethernet test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,101 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
-// #include <ICMPPing.h>
 
+namespace DigitalPins {
+  constexpr int LED_PIN = 13;
+  constexpr int ETHERNET_PIN = 5;
+  constexpr int SDCARD_PIN = 7;
+  constexpr int DS18B2_PIN = 9;
+}
 
-//TODO:
-//1. Change from global
-//
+namespace AnalogPins {
+
+}
 
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // NOLINT
 byte ip[] = { 172, 16, 18, 181 };  
-IPAddress pingAddr(74,125,26,147); 
-
+EthernetServer server(80);
 
 void setup() {
-  EthernetClass::begin(mac, ip);
 
-  const int BAUD_RATE = 9600;
-  Serial.begin(BAUD_RATE);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Ethernet WebServer Example");
+  EthernetClass::init(DigitalPins::ETHERNET_PIN);
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 }
 
-void loop() {
 
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an HTTP request ends with a blank line
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the HTTP request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard HTTP response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
